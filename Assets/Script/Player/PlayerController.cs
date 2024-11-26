a5ee30ab0557e1d2c60ce25592a6424fe456337e
aloisz <alois.tupinon@gmail.com>
2024-11-26 17:44:21 +0100

    ca bouge

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class PlayerController : Actor
 {
     public static PlayerController instance;
-
+    
+    [SerializeField] private Vector2 playerPos = Vector2.one;
+    [SerializeField] private bool isGoingUp = false;
     private void Awake()
     {
         if (instance == null)
@@ -16,14 +18,18 @@ public class PlayerController : Actor
 
     public override void MoveRight()
     {
+        if(playerPos.x >= 4) return;
         Debug.Log($"Move Right !");
+        playerPos += Vector2.right;
         
         //TODO : Draw the room you at, update position on array 
     }
 
     public override void MoveLeft()
     {
+        if(playerPos.x <= 0) return;
         Debug.Log($"MoveLeft !");
+        playerPos += -Vector2.right;
     }
 
     public override void Search()
@@ -33,6 +39,9 @@ public class PlayerController : Actor
 
     public override void TakeStair()
     {
+        if (playerPos.y is <= 0 or >= 3) return;
+
         Debug.Log($"TakeStair !");
+        playerPos += isGoingUp ? Vector2.up : -Vector2.up;
     }
 }
