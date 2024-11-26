74162900209d57b438b59f182579c108be852ffe
aloisz <alois.tupinon@gmail.com>
2024-11-26 17:21:42 +0100

    room OBJ

--- Assets/Script/Procedural Generation/RoomObj.cs
+++ Assets/Script/Procedural Generation/RoomObj.cs
@@ -7,5 +7,47 @@ namespace Script.Procedural_Generation
         //TODO faire une variable "content" si jamais l'obj contient qqchose
         
         //TODO faire une variable pour savoir si il faut un obj pour intéragir avec cette objet
+
+        // Search
+        [SerializeField] private bool canBeSearch;
+        
+        // Contain
+        private bool doContain = false; // is OBJ contain Something
+        private string containDescription; // description of what it contains
+        private RoomObj objToGive;
+
+
+        // Value of canBeSearch
+        public bool GetCanBeSearch()
+        {
+            return canBeSearch;
+        }
+
+        // Set on room initialisation 
+        public void SetWhatObjContain(string description, RoomObj OBJ)
+        {
+            doContain = true;
+            objToGive = OBJ;
+        }
+
+        /// <summary>
+        /// Call when obj is inspected 
+        /// </summary>
+        public void SearchOBJ()
+        {
+            switch (doContain)
+            {
+                case true:
+                    Debug.Log($"it contain {containDescription}");
+                    
+                    if(objToGive == null)break;
+                    Debug.Log($"You receive {objToGive}");
+                    break;
+                
+                default:
+                    Debug.Log("Nothing");
+                    break;
+            }
+        }
     }
 }
