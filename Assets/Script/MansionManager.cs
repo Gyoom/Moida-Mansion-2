4df35402b427430e7125efaaba664654c2f7ec74
Pierre Ferrari <pierreferrari22@gmail.com>
2024-11-27 11:51:34 +0100

    Mansion Manager

---
+++ Assets/Script/MansionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Script.Procedural_Generation;
+using UnityEngine;
+
+namespace Script
+{
+    public class MansionManager : MonoBehaviour
+    {
+        public static MansionManager Instance;
+
+        private Room[,] m_mansionMatrix = new Room[4, 3];
+        
+        public List<RoomObj> CommonRoomObj = new List<RoomObj>();
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            MansionGenerator generator = new MansionGenerator();
+            generator.GenerateMansion(m_mansionMatrix);
+            m_mansionMatrix[generator.EntranceColumnIndex, 1].DisplayRoom(); // Display Entrance
+        }
+    }
+}
