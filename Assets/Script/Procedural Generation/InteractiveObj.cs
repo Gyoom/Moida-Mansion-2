b4d8cf61241bdb27212cb189487e7bba34b9f122
aloisz <alois.tupinon@gmail.com>
2024-12-10 15:07:45 +0100

    child dialogue

--- Assets/Script/Procedural Generation/InteractiveObj.cs
+++ Assets/Script/Procedural Generation/InteractiveObj.cs
@@ -1,7 +1,10 @@
-﻿namespace Script.Procedural_Generation
+﻿using System.Runtime.CompilerServices;
+
+namespace Script.Procedural_Generation
 {
     public class InteractiveObj
     {
         public bool IsKid;
+        public string[] dialogue;
     }
 }
